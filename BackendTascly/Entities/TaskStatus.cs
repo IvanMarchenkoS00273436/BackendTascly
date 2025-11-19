d06cb02eb7c1e03708315d57546301e4e4cc664b
Ivan1 <S00273436@atu.ie>
2025-11-19 11:46:55 +0000

    Enhanced the `TaskStatus` class by adding data annotations:

--- BackendTascly/Entities/TaskStatus.cs
+++ BackendTascly/Entities/TaskStatus.cs
@@ -1,8 +1,14 @@
-﻿namespace BackendTascly.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendTascly.Entities
 {
     public class TaskStatus
     {
+        [Key]
         public Int16 Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
     }
 }
