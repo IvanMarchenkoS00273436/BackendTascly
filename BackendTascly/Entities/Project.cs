f676e419c9a7ac6dad84010f3665981e172d4024
Ivan1 <S00273436@atu.ie>
2025-11-19 11:55:51 +0000

    Updated the `Project` entity to include data annotations for validation and added new properties for better relational mapping. Key changes include:

--- BackendTascly/Entities/Project.cs
+++ BackendTascly/Entities/Project.cs
@@ -1,10 +1,25 @@
-﻿namespace BackendTascly.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendTascly.Entities
 {
     public class Project
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required, StringLength(100)]
         public string Name { get; set; }
+
+        [StringLength(1000)]
         public string Description { get; set; }
-        public User Owner { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+        public virtual User Owner { get; set; }
+
+        public virtual ICollection<User> Members { get; set; } = new List<User>();
+        public virtual ICollection<TaskStatus> TaskStatuses { get; set; } = new List<TaskStatus>();
+        public virtual ICollection<TaskImportance> TaskImportances { get; set; } = new List<TaskImportance>();
+        public virtual ICollection<PTask> Tasks { get; set; } = new List<PTask>();
     }
 }
