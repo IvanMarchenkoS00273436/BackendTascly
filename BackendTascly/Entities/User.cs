d867f1f651883f43cfae16b6737a08a29e91a348
Ivan1 <S00273436@atu.ie>
2025-11-20 14:17:31 +0000

    User entity update validation attributes

--- BackendTascly/Entities/User.cs
+++ BackendTascly/Entities/User.cs
@@ -1,11 +1,22 @@
-﻿namespace BackendTascly.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackendTascly.Entities
 {
     public class User
     {
+        [Key]
         public Guid Id { get; set; }
+
+        [Required, EmailAddress]
         public string Username { get; set; } = string.Empty;
+
+        [StringLength(100), MinLength(2)]
         public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(100), MinLength(2)]
         public string LastName { get; set; } = string.Empty;
+
+        [Required]
         public string PasswordHash { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public string? RefreshToken { get; set; }
