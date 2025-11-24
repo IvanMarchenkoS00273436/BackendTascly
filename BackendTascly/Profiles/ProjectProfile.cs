9bcd61fa73017b1c903d644022a8aa257ad68d2d
Ivan Marchenko <vania9023@gmail.com>
2025-11-24 22:19:37 +0000

    Add AutoMapper profile for Project to GetProject mapping

---
+++ BackendTascly/Profiles/ProjectProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BackendTascly.Entities;
+using BackendTascly.Entities.ModelsDto.ProjectsDtos;
+
+namespace BackendTascly.Profiles
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<Project, GetProject>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
+                .ForMember(dest => dest.Description, src => src.MapFrom(x => x.Description))
+                .ForMember(dest => dest.OwnerId, src => src.MapFrom(x => x.OwnerId));
+        }
+    }
+}
