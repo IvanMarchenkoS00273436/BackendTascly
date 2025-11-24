17b4564724ee8f73c1beb3e5fb8567422d9dcfee
Ivan Marchenko <vania9023@gmail.com>
2025-11-24 22:22:34 +0000

    add mapping project from postproject

--- BackendTascly/Profiles/ProjectProfile.cs
+++ BackendTascly/Profiles/ProjectProfile.cs
@@ -8,11 +8,17 @@ namespace BackendTascly.Profiles
     {
         public ProjectProfile()
         {
+            // Mapping between Project and GetProject DTO (GetProject from Project)
             CreateMap<Project, GetProject>()
                 .ForMember(dest => dest.Id, src => src.MapFrom(x => x.Id))
                 .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
                 .ForMember(dest => dest.Description, src => src.MapFrom(x => x.Description))
                 .ForMember(dest => dest.OwnerId, src => src.MapFrom(x => x.OwnerId));
+
+            // Mapping between PostProject DTO and Project (Project from PostProject)
+            CreateMap<PostProject, Project>()
+                .ForMember(dest => dest.Name, src => src.MapFrom(x => x.Name))
+                .ForMember(dest => dest.Description, src => src.MapFrom(x => x.Description));
         }
     }
 }
