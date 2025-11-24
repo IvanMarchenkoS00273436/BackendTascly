5c606ea480a812bf1e1e8346ea3b9abff39b6d3f
Ivan Marchenko <vania9023@gmail.com>
2025-11-24 22:32:02 +0000

    Add ProjectService and IProjectService interface

---
+++ BackendTascly/Services/IProjectService.cs
@@ -0,0 +1,12 @@
+﻿using BackendTascly.Entities;
+
+namespace BackendTascly.Services
+{
+    public interface IProjectService
+    {
+        Task<bool> CreateProjectAsync(Project postProject);
+        Task<bool> DeleteProjectAsync(Guid projectId);
+        Task<List<Project>> GetAllProjectsByOwnerIdAsync(Guid ownerId);
+        Task<Project?> GetProjectByIdAsync(Guid projectId);
+    }
+}
---
+++ BackendTascly/Services/ProjectService.cs
@@ -0,0 +1,28 @@
+﻿using BackendTascly.Entities;
+using BackendTascly.Repositories;
+
+namespace BackendTascly.Services
+{
+    public class ProjectService(ProjectsRepository projectsRepository) : IProjectService
+    {
+        public Task<bool> CreateProjectAsync(Project project)
+        {
+            return projectsRepository.AddProjectAsync(project);
+        }
+
+        public Task<bool> DeleteProjectAsync(Guid projectId)
+        {
+            return projectsRepository.DeleteProjectAsync(projectId);
+        }
+
+        public Task<List<Project>> GetAllProjectsByOwnerIdAsync(Guid ownerId)
+        {
+            return projectsRepository.GetAllProjectsByOwnerId(ownerId);
+        }
+
+        public Task<Project?> GetProjectByIdAsync(Guid projectId)
+        {
+            return projectsRepository.GetProjectById(projectId);
+        }
+    }
+}
