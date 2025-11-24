1c033524a97751fd86a668eba6959de29d858540
Ivan Marchenko <vania9023@gmail.com>
2025-11-24 21:32:49 +0000

    Add ProjectsRepository and IProjectsRepository interface

---
+++ BackendTascly/Repositories/IProjectsRepository.cs
@@ -0,0 +1,12 @@
+﻿using BackendTascly.Entities;
+
+namespace BackendTascly.Repositories
+{
+    public interface IProjectsRepository
+    {
+        Task<List<Project>> GetAllProjectsByOwnerId(Guid ownerId);
+        Task<Project?> GetProjectById(Guid projectId);
+        Task AddProjectAsync(Project project);
+        Task DeleteProjectAsync(Guid projectId);
+    }
+}
---
+++ BackendTascly/Repositories/ProjectsRepository.cs
@@ -0,0 +1,45 @@
+﻿using BackendTascly.Data;
+using BackendTascly.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendTascly.Repositories
+{
+    public class ProjectsRepository(TasclyDbContext context) : IProjectsRepository
+    {
+        public async Task AddProjectAsync(Project project)
+        {
+            await context.Projects.AddAsync(project);
+        }
+
+        public async Task DeleteProjectAsync(Guid projectId)
+        {
+            var project = await context.Projects.FindAsync(projectId);
+            context.Projects.Remove(project);
+        }
+
+        public async Task<List<Project>> GetAllProjectsByOwnerId(Guid ownerId)
+        {
+            var projects = await context.Projects
+                .Include(p => p.Members)
+                .Include(p => p.TaskStatuses)
+                .Include(p => p.TaskImportances)
+                .Include(p => p.Tasks)
+                .Include(p => p.Roles)
+                .Where(p => p.OwnerId == ownerId).ToListAsync();
+            return projects;
+        }
+
+        public Task<Project?> GetProjectById(Guid projectId)
+        {
+            var project = context.Projects
+                .Include(p => p.Members)
+                .Include(p => p.TaskStatuses)
+                .Include(p => p.TaskImportances)
+                .Include(p => p.Tasks)
+                .Include(p => p.Roles)
+                .FirstOrDefaultAsync(p => p.Id == projectId);
+
+            return project;
+        }
+    }
+}
