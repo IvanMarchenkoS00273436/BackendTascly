11d4a5717dbf673fea43c040c1aae284d2698897
Oleksandr274 <s00274217@atu.ie>
2026-02-21 00:18:10 +0000

    update CreateTask functionality to allow nullable fields in the request

--- BackendTascly/Data/ModelsDto/TaskDtos/PostTask.cs
+++ BackendTascly/Data/ModelsDto/TaskDtos/PostTask.cs
@@ -7,11 +7,11 @@ namespace BackendTascly.Data.ModelsDto.TaskDtos
         [Required, StringLength(100)]
         public string Name { get; set; }
         [StringLength(1000)]
-        public string Description { get; set; }
-        public DateTime StartDate { get; set; } //optional
-        public DateTime DueDate { get; set; } //optional
-        public Int16 StatusId { get; set; } //required with default
-        public Int16 ImportanceId { get; set; } //required with default
+        public string? Description { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? DueDate { get; set; } 
+        public Int16 StatusId { get; set; } 
+        public Int16 ImportanceId { get; set; } 
         public Guid? AssigneeId { get; set; }
     }
 }
