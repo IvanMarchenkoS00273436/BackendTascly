9fbcf4257cae3bed9f90cc498d4165e9f1312166
ryanmc1407 <ryanmcclelland@msn.com>
2026-02-27 19:05:25 +0000

    Added AiGenerateRequest DTO

---
+++ BackendTascly/Data/ModelsDto/AiDtos/AiGenerateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendTascly.Data.ModelsDto.AiDtos
+{
+    public class AiGenerateRequest
+    {
+        public string Prompt { get; set; }  = string.Empty;
+        public Guid ProjectId { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
