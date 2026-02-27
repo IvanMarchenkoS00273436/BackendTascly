37d83b25ebd3f19f8ba85c2a9faaf17cc2930ed0
ryanmc1407 <ryanmcclelland@msn.com>
2026-02-27 20:22:43 +0000

    Added AiService

--- BackendTascly/Services/IAiService.cs
+++ BackendTascly/Services/IAiService.cs
@@ -1,6 +1,9 @@
-﻿namespace BackendTascly.Services
+﻿using BackendTascly.Data.ModelsDto.AiDtos;
+namespace BackendTascly.Services
 {
-    public class IAiService
+    public interface IAiService
     {
+        Task<AiGenerateResponse> GenerateTasksAsync(AiGenerateRequest request);
+        Task<bool> BulkCreateTasksAsync(BulkCreateRequest request, Guid userId);
     }
 }
