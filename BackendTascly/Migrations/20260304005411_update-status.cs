cf9c6dfed453fa088e7a3fb415e52c3fa9b61364
Oleksandr274 <s00274217@atu.ie>
2026-03-04 02:14:01 +0000

    add migration: update taskStatus tbl to contain a reference to NextStatus

---
+++ BackendTascly/Migrations/20260304005411_update-status.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackendTascly.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatestatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "NextStatusId",
+                table: "TaskStatuses",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TaskStatuses_NextStatusId",
+                table: "TaskStatuses",
+                column: "NextStatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskStatuses_TaskStatuses_NextStatusId",
+                table: "TaskStatuses",
+                column: "NextStatusId",
+                principalTable: "TaskStatuses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskStatuses_TaskStatuses_NextStatusId",
+                table: "TaskStatuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TaskStatuses_NextStatusId",
+                table: "TaskStatuses");
+
+            migrationBuilder.DropColumn(
+                name: "NextStatusId",
+                table: "TaskStatuses");
+        }
+    }
+}
