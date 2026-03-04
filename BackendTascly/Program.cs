156560a5c74c3cee2184299f380c4ea6493a5457
Ryan Mc Clelland <ryanmcclelland@msn.com>
2026-03-04 17:41:30 +0000

    Fixed program.cs

--- BackendTascly/Program.cs
+++ BackendTascly/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddScoped<ITaskService, TaskService>();
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<IAiService, AiService>();
 builder.Services.AddScoped<IInvitationRepository, InvitationRepository>();
-builder.Services.AddHttpClient("Grow");
+builder.Services.AddHttpClient("Groq");
 builder.Services.AddHttpClient("Lambda");
 
 
